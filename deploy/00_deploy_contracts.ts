75cfaf80acbe08630264ad4efb70018404a8d9c8
Julian Yap <julianokyap@gmail.com>
2021-09-12 15:49:13 -0700

    Add rewritten deployment script

---
+++ deploy/00_deploy_contracts.ts
@@ -0,0 +1,33 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { network } from 'hardhat';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {
+    deployments: { deploy },
+    getNamedAccounts,
+  } = hre;
+  const { deployer } = await getNamedAccounts();
+  let claimPrice = "88000000000000000000";
+
+  if (network.name == "kovan") {
+    claimPrice = "88000000000000000";
+  }
+  if (network.name == "localhost") {
+    claimPrice = "88000000000000000";
+  }
+
+  const burner = await deploy('Burner', {
+    from: deployer,
+    log: true,
+  });
+
+  const n = await deploy('N', {
+    from: deployer,
+    args: [burner.address, claimPrice],
+    log: true,
+  });
+};
+
+export default func;
+func.tags = ['Burner', 'N'];
