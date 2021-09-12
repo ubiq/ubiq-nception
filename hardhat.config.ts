7a2fc1f3fe914276836eee69fdc13f4637f75448
Julian Yap <julianokyap@gmail.com>
2021-09-12 15:46:48 -0700

    Convert to hardhat-deploy plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,12 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import 'hardhat-deploy';
+
+import * as dotenv from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/config';
+
+dotenv.config();
+/* This loads the variables in your .env file to `process.env` */
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -9,7 +16,28 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-export default {
+const { ALCHEMY_KOVAN_API_KEY } = process.env;
+
+function getMnemonic(networkName?: string): string {
+  if (networkName) {
+    const mnemonic = process.env['MNEMONIC_' + networkName.toUpperCase()];
+    if (mnemonic && mnemonic !== '') {
+      return mnemonic;
+    }
+  }
+
+  const mnemonic = process.env.MNEMONIC;
+  if (!mnemonic || mnemonic === '') {
+    return 'test test test test test test test test test test test junk';
+  }
+  return mnemonic;
+}
+
+function accounts(networkName?: string): {mnemonic: string} {
+  return {mnemonic: getMnemonic(networkName)};
+}
+
+const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
@@ -19,18 +47,37 @@ export default {
         version: "0.4.20",
       },
     ],
-  },
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 800,
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+      evmVersion: "istanbul",
     },
-    evmVersion: "istanbul",
   },
   networks: {
+    localhost: {
+      accounts: accounts(),
+    },
     mainnet: {
-      url: "https://rpc.octano.dev",
+      url: "http://127.0.0.1:8588",
       chainId: 8,
+      gasPrice: 11000000000,
+    },
+    kovan: {
+      url: "https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_KOVAN_API_KEY}",
+      chainId: 42,
+      accounts: accounts('kovan'),
+      gasPrice: 1100000000,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      "mainnet": '0xc5070A5CB93F4497240a57969485C0FbF5c2ee3A',
+      "kovan": '0x2EF7526b624136b2EE8B426c620C3E6b3997F24e',
     },
   },
 };
+
+export default config;
