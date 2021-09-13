34845eb4dc488f6e1f04d2c5726df13fed5adcb5
Julian Yap <julianokyap@gmail.com>
2021-09-13 10:55:15 -0700

    Update test cases

--- test/nCeption.ts
+++ test/nCeption.ts
@@ -11,6 +11,7 @@ describe("nCeption - The dream is real", function () {
   let owner: SignerWithAddress;
   let address1: SignerWithAddress;
   let address2: SignerWithAddress;
+  const nClaimPrice = 88000000000000000000n;
 
   beforeEach(async () => {
     const Burner = await ethers.getContractFactory("Burner");
@@ -18,7 +19,7 @@ describe("nCeption - The dream is real", function () {
 
     const N = await ethers.getContractFactory("N");
     [owner, address1, address2] = await ethers.getSigners();
-    nContract = await N.deploy(burnerContract.address, 88000000000000000000n);
+    nContract = await N.deploy(burnerContract.address, nClaimPrice);
   });
 
   it("Should set the right owner", async () => {
@@ -26,11 +27,11 @@ describe("nCeption - The dream is real", function () {
   });
 
   it("Should fail to mint an N for Token ID 0", async () => {
-    await expect(nContract.claim(0, { value: 88000000000000000000n })).to.be.revertedWith("Token ID invalid");
+    await expect(nContract.claim(0, { value: nClaimPrice })).to.be.revertedWith("Token ID invalid");
   });
 
   it("Should fail to mint an N for Token ID 8889", async () => {
-    await expect(nContract.claim(8889, { value: 88000000000000000000n })).to.be.revertedWith("Token ID invalid");
+    await expect(nContract.claim(8889, { value: nClaimPrice })).to.be.revertedWith("Token ID invalid");
   });
 
   it("Should fail to mint an N when incorrect value sent", async () => {
@@ -38,14 +39,19 @@ describe("nCeption - The dream is real", function () {
   });
 
   it("Should mint an N with Token ID 1", async () => {
-    await expect(nContract.claim(1, { value: 88000000000000000000n })).to.emit(
-      nContract,
-      "Transfer"
-    );
+    await expect(nContract.claim(1, { value: nClaimPrice }))
+      .to.emit(nContract, "Transfer")
+      .withArgs(ethers.constants.AddressZero, owner.address, 1);
   });
 
   it("Should fail to mint an N ID that has already been minted", async () => {
-    await nContract.claim(1, { value: 88000000000000000000n });
-    await expect(nContract.claim(1, { value: 88000000000000000000n })).to.be.revertedWith("ERC721: token already minted");
+    await nContract.claim(1, { value: nClaimPrice });
+    await expect(nContract.claim(1, { value: nClaimPrice })).to.be.revertedWith("ERC721: token already minted");
+  });
+
+  it("Should increment total supply", async () => {
+    await nContract.claim(1, { value: nClaimPrice });
+    await nContract.claim(2, { value: nClaimPrice });
+    expect(await nContract.totalSupply()).to.be.equal('0x02');
   });
 });
